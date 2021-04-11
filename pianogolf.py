33d8a20c90190dd90de1483316609c3b49c5a7f0
Jeremy Schmidt <8888694+nocturnalBadger@users.noreply.github.com>
2021-04-11 11:47:59 -0400

    comments

--- pianogolf.py
+++ pianogolf.py
@@ -4,18 +4,23 @@ import uinput
 from dataclasses import dataclass
 
 
-
+# Name or prefix for the midi input device to use
 MIDI_DEVICE = "MPK Mini"
 
+# Midi status codes for key functions
+# https://www.midi.org/specifications-old/item/table-2-expanded-messages-list-status-bytes
 MIDI_NOTE_ON = 144
 MIDI_NOTE_OFF = 128
 MIDI_CONTROL_CHANGE = 176
 
+# Definitions for side-to-side controls
 COARSE_KNOB = 1
 FINE_KNOB = 2
 COARSE_MOVEMENT = 50
 FINE_MOVEMENT = 5
 
+# Definitions for key range (my AKAI MPK mini goes from c3 to c5)
+# This makes middle c exactly half power
 LOWEST_KEY = 48
 HIGHEST_KEY = 72
 
@@ -24,6 +29,10 @@ VELOCITY_MODE = False
 
 
 def get_midi_device():
+    """
+    Search for a midi device with the substring set in MIDI_DEVICE 
+    return the device id (int)
+    """
     for i in range(pygame.midi.get_count()):
         r = pygame.midi.get_device_info(i)
         (interf, name, is_input, is_output, opened) = r
@@ -33,6 +42,10 @@ def get_midi_device():
     raise ValueError("Midi device not found")
 
 def hit_ball(velocity):
+    """
+    Script mouse movements to make a shot with the given velocity
+    :param velocity: relative shot power from 0 to 128
+    """
     FULL_BEANS = -1000
 
     power = int(FULL_BEANS * (velocity / 128))
@@ -52,6 +65,7 @@ def hit_ball(velocity):
     device.emit(uinput.BTN_LEFT, 0)
 
 def handle_event(status_code, data_0, data_1, control_state):
+    """ Handle a midi event """
     coarse_last_pos, fine_last_pos = control_state
     if status_code == MIDI_CONTROL_CHANGE:
         dial_num = data_0
@@ -79,6 +93,7 @@ def handle_event(status_code, data_0, data_1, control_state):
     return (coarse_last_pos, fine_last_pos)
 
 def loop():
+    """ Main event loop """
     # Record knob positions
     control_state = (0, 0)
     while True:
